6f417a110c769377842a4fffb11550ab5fa0214b
Mona Lisa <daint@gmail.com>
2022-05-03 09:22:22 +0700

    s

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const http = require("http");
+
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+    res.send("Vãi lồn");
+});
+
+server.listen(3000, () => {
+    console.log('daint');
+});
