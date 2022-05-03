c498419736ed960ccbf5a7a9a1213d6d5e6641e5
Mona Lisa <daint@gmail.com>
2022-05-03 10:12:27 +0700

    78

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const app = express();
 
 const http = require("http");
 
+
 const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+
+const io = new Server(server);
+
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("Đại socket");
 });
 
 server.listen(3000, () => {
